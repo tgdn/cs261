725ed7540a59b4b9230da883e985839212962ed3
Thomas Gak Deluen <tgdn45@gmail.com>
2017-02-05 20:36:46 +0000

    Added redux provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
+import store from './store'
 import App from './components/app'
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('app')
 )
