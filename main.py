b139327983267d4eecbfaba47c3509e59200a2d2
Thomas Gak Deluen <tgdn45@gmail.com>
2017-02-13 15:18:25 +0000

    Added help and improved argparse

--- main.py
+++ main.py
@@ -6,12 +6,31 @@ import argparse
 from purple import App
 
 def main():
-    parser = argparse.ArgumentParser(description='Purple trade backend')
-    parser.add_argument('--init-db', action='store_true')
-    parser.add_argument('--reset-db', action='store_true')
-    parser.add_argument('-f', '--file', type=str)
-    parser.add_argument('-s', '--stream-url', type=str)
-    parser.add_argument('-p', '--port', type=int)
+    parser = argparse.ArgumentParser(description='Purple trading backend')
+    group = parser.add_mutually_exclusive_group()
+
+    parser.add_argument(
+        '--init-db', action='store_true',
+        help='Create database, tables and indexes.\
+        Will have no effect if the db already exists.'
+    )
+    parser.add_argument(
+        '--reset-db', action='store_true',
+        help='Delete database. WARNING: This will delete everything!'
+    )
+
+    group.add_argument(
+        '-f', '--file', type=argparse.FileType('r'),
+        help='File input to read trade data from.'
+    )
+    group.add_argument(
+        '-s', '--stream-url', type=str,
+        help='URL to a stream of data.'
+    )
+    parser.add_argument(
+        '-p', '--port', type=int, default=80,
+        help='Specify a port to given stream url. (default: 80)'
+    )
 
     args = parser.parse_args()
     App(args)
