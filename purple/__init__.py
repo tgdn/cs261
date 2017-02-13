4ef2cacc2a5650cfaa685ea746d904b629b8c77b
Thomas Gak Deluen <tgdn45@gmail.com>
2017-02-13 15:18:38 +0000

    Added added trades info

--- purple/__init__.py
+++ purple/__init__.py
@@ -4,8 +4,7 @@ import os
 import sys
 import fcntl
 import socket
-import requests
-from contextlib import contextmanager, closing
+from contextlib import contextmanager
 
 import rethinkdb as r
 from rethinkdb.errors import RqlRuntimeError, RqlDriverError
@@ -15,6 +14,17 @@ from purple.finance import Trade
 RDB_HOST = 'localhost'
 RDB_PORT = '28015'
 PURPLE_DB = 'purple'
+
+# write to stdout
+def stdout_write(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
+
+# reset line to write on same line again
+def reset_line():
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+
 
 class App:
     def __init__(self, args):
@@ -29,34 +39,59 @@ class App:
             port = args.port or 80
             self.from_stream(url=args.stream_url, port=port)
 
-        print args
+    def from_file(self, f):
+        '''
+        Read file containing trading data.
+        Read each line and insert in DB.
+        File is opened by argparse.
+        '''
+        # set non blocking i/o
+        fd = f.fileno()
+        flag = fcntl.fcntl(fd, fcntl.F_GETFD)
+        fcntl.fcntl(fd, fcntl.F_SETFL, flag |  os.O_NONBLOCK)
+        next(f) # skip header row
 
-    def from_file(self, filename):
-        with open(filename, 'r') as f:
-            # set non blocking i/o
-            fd = f.fileno()
-            flag = fcntl.fcntl(fd, fcntl.F_GETFD)
-            fcntl.fcntl(fd, fcntl.F_SETFL, flag |  os.O_NONBLOCK)
-            next(f) # skip header row
+        tradecount = 0
+        with self.get_reql_connection(True) as conn:
+            for line in f:
+                # insert
+                Trade(line).save(conn)
 
-            with self.get_reql_connection(PURPLE_DB) as conn:
-                for line in f:
-                    Trade(line).save(conn)
+                # inform user
+                tradecount = tradecount + 1
+                stdout_write('Trades: {} (Ctrl-C to stop)'.format(tradecount))
+                reset_line()
+        try:
+            f.close()
+        except:
+            pass
 
     def from_stream(self, url, port=80):
-        # TODO: read from stream
-        print 'hello'
-        server_name = (url, port)
+        '''
+        Read live stream of trading data
+        and insert into DB.
+        '''
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(server_name)
+        sock.connect((url, port))
 
+        firstline = True
+        tradecount = 0
         line = ''
-        with self.get_reql_connection() as conn:
+        with self.get_reql_connection(True) as conn:
             while 1:
+                # read character
                 char = sock.recv(1)
                 if char == '\n':
-                    #print line
-                    Trade(line).save(conn)
+                    if firstline:
+                        firstline = False
+                    else:
+                        # insert
+                        Trade(line).save(conn)
+
+                        # inform user
+                        tradecount = tradecount + 1
+                        stdout_write('Trades: {} (Ctrl-C to stop)'.format(tradecount))
+                        reset_line()
                     line = ''
                 else:
                     line = line + char
@@ -84,11 +119,11 @@ class App:
                 print 'Database setup complete.'
 
     @contextmanager
-    def get_reql_connection(db=None):
+    def get_reql_connection(self, db=False):
         """
         Make rdb connection available as context manager generator.
         ie:
-        with self.get_reql_connection('somedb') as conn:
+        with self.get_reql_connection(db=True) as conn:
             r.table('sometable').run(conn)
         """
         try:
