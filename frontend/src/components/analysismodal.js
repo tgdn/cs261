7679f11959fac10f180c2c18fbafe92f7f3cd4d6
Thomas Gak Deluen <tgdn45@gmail.com>
2017-02-21 13:56:59 +0000

    Done most of the modal layout and functionality

--- frontend/src/components/analysismodal.js
+++ frontend/src/components/analysismodal.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button, Modal, Header, Icon } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { Grid, Button, Modal, Header, Icon, Input, Form } from 'semantic-ui-react'
 import UploadButton from './uploadbutton'
 
 class AnalysisModal extends React.Component {
@@ -36,26 +37,85 @@ class AnalysisModal extends React.Component {
                 open={this.state.opened}
                 onClose={this.handleClose}
                 size='small'
+                closeIcon={true}
             >
                 <Header icon='terminal' content='Analyse trades' />
                 <Modal.Content>
-                    <p>Hello</p>
-                    <button>Hello</button>
-                    <UploadButton uploadTo='/upload'>
-                        Upload
-                    </UploadButton>
+                    <Grid divided='vertically'>
+                        <Grid.Row columns={2}>
+                            <Grid.Column>
+                                <Header size='medium'>Static file</Header>
+                                <UploadButton
+                                    uploadTo='/upload'
+                                    handleSuccess={this.props.updateFile}
+                                    name='file'
+                                    acceptedTypes={['text/csv']}
+                                >
+                                    <Icon name='cloud upload' />
+                                    Select file...
+                                </UploadButton>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Header size='medium'>Live stream</Header>
+                                <Form as='div'>
+                                    <Form.Field>
+                                        <Input
+                                            placeholder='Stream url'
+                                            onChange={this.props.updateStream}
+                                            value={this.props.streamUrl}
+                                        />
+                                    </Form.Field>
+                                    <Form.Field>
+                                        <Input
+                                            placeholder='Port'
+                                            onChange={this.props.updatePort}
+                                            value={this.props.port}
+                                        />
+                                    </Form.Field>
+                                    <Button secondary type='button'>
+                                        <Icon name='laptop' /> Start analysing stream
+                                    </Button>
+                                </Form>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </Modal.Content>
-                <Modal.Actions>
-                    <Button onClick={this.handleClose}>
-                        <Icon name='remove' /> Cancel
-                    </Button>
-                    <Button secondary>
-                        <Icon name='checkmark' /> Yes
-                    </Button>
-                </Modal.Actions>
             </Modal>
         )
     }
 }
 
-export default AnalysisModal
+export default connect(
+    state => ({
+        file: state.analysis.file,
+        streamUrl: state.analysis.streamUrl,
+        port: state.analysis.port
+    }),
+    dispatch => ({
+        updateFile: (res) => {
+            console.log(res)
+            // dispatch({
+            //     type: 'UPDATE_FILE',
+            //     data: {
+            //         file: res.,
+            //     }
+            // })
+        },
+        updateStream: (e) => {
+            dispatch({
+                type: 'UPDATE_STREAM',
+                data: {
+                    streamUrl: e.target.value,
+                }
+            })
+        },
+        updatePort: (e) => {
+            dispatch({
+                type: 'UPDATE_PORT',
+                data: {
+                    port: e.target.value,
+                }
+            })
+        }
+    })
+)(AnalysisModal)
