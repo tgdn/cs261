724a7f248e6db7695a0bfe23ee42b4f18d8a3925
Thomas Gak Deluen <tgdn45@gmail.com>
2017-02-27 16:37:23 +0000

    Started redesigning home page

--- frontend/src/components/pages/home.js
+++ frontend/src/components/pages/home.js
@@ -52,51 +52,22 @@ class HomePage extends React.Component {
     }
 
     render() {
-        const tradeCount = parseInt(this.state.trades.length, 10)
-
-        const items = [
-          { label: 'Trades', value: tradeCount },
-          { label: 'Issues', value: '0' },
-          { label: 'Unresolved', value: '0' },
-        ]
-
-        const groupedTrades = groupBy(this.state.trades, 'symbol')
-
         return (
             <div>
-                <Dimmer
-                    active={!this.state.loaded}
-                    page
-                >
-                    <Loader size='large'>Loading latest data</Loader>
-                </Dimmer>
                 <Grid padded stackable>
-                    <Grid.Row color='black'>
-                        <Grid.Column width={16}>
-                            <Statistic.Group
-                                inverted
-                                widths='three'
-                                items={items}
-                                color='green'
-                            />
-                        </Grid.Column>
-                    </Grid.Row>
                     <Grid.Column width={4}>
-                        <Menu vertical>
-                            {map(groupedTrades, (trades, symbol) => (
+                        <Menu vertical fluid text>
+                            {/* map(groupedTrades, (trades, symbol) => (
                                 <Menu.Item key={symbol}>
                                     <Menu.Header>{symbol}</Menu.Header>
                                     <Menu.Menu>
                                         <Menu.Item name={`${trades.length} trades`} />
                                     </Menu.Menu>
                                 </Menu.Item>
-                            ))}
+                            ))*/}
                         </Menu>
                     </Grid.Column>
                     <Grid.Column width={12}>
-                        <Container textAlign='left'>
-                            <Link to='/some/random/url'>Some random url</Link>
-                        </Container>
                         <Container textAlign='center'>
                             <Icon color='green' name='check circle' size='massive' />
                             <Header as='h2' color='green'>No issues</Header>
@@ -109,15 +80,12 @@ class HomePage extends React.Component {
 }
 
 HomePage.propTypes = {
-    trades: React.PropTypes.object, // eslint-disable-line
 }
 
 HomePage.defaultProps = {
-    trades: [],
 }
 
 export default connect(
     state => ({
-        trades: state.db.trades,
     }),
 )(HomePage)
