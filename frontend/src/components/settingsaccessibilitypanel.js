1ca4c2d7c4acffba7244e2559967831fd3e136ac
Thomas Gak Deluen <tgdn45@gmail.com>
2017-02-27 13:03:10 +0000

    Functional component for accessibility panel

--- frontend/src/components/settingsaccessibilitypanel.js
+++ frontend/src/components/settingsaccessibilitypanel.js
@@ -13,47 +13,41 @@ import {
     Divider
 } from 'semantic-ui-react'
 
-class SettingsAnalysisPanel extends React.Component {
-    render() {
-        const {
-            updateSetting,
-            updateValue
-        } = this.props
-
-        return (
-            <Grid divided='vertically'>
-                <Grid.Row columns={2}>
-                    <Grid.Column>
-                        <Checkbox
-                            toggle
-                            label='Invert colours'
-                            onChange={(e, { checked }) => {
-                                updateSetting('inverse', checked)
-                                updateValue('inverse', checked)
-                            }}
-                            checked={this.props.inverse}
-                        />
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Checkbox
-                            toggle
-                            label='Large text'
-                            onChange={(e, { checked }) => {
-                                //updateSetting('inverse', checked)
-                                //updateValue('inverse', checked)
-                            }}
-                        />
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        )
-    }
-}
+const SettingsAnalysisPanel = ({
+    updateSetting, updateValue, inverse, largetext
+}) => (
+    <Grid divided='vertically'>
+        <Grid.Row columns={2}>
+            <Grid.Column>
+                <Checkbox
+                    toggle
+                    label='Invert colours'
+                    onChange={(e, { checked }) => {
+                        updateSetting('inverse', checked)
+                        updateValue('inverse', checked)
+                    }}
+                    checked={inverse}
+                />
+            </Grid.Column>
+            <Grid.Column>
+                <Checkbox
+                    toggle
+                    label='Large text'
+                    onChange={(e, { checked }) => {
+                        updateSetting('largetext', checked)
+                        updateValue('largetext', checked)
+                    }}
+                    checked={largetext}
+                />
+            </Grid.Column>
+        </Grid.Row>
+    </Grid>
+)
 
 export default connect(
     state => ({
         settings: state.db.settings,
-        mode: state.settings.mode,
         inverse: state.settings.inverse,
+        largetext: state.settings.largetext
     }),
 )(SettingsAnalysisPanel)
