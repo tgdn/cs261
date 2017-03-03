75e9028cfcf1c7014bf6893dad491c2446fa004d
Thomas Gak Deluen <tgdn45@gmail.com>
2017-03-03 21:49:33 +0000

    Made header logo link back to dashboard

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { Link } from 'react-router'
+import { Link } from 'react-router'
 import { Menu } from 'semantic-ui-react'
 
 import SettingsModal from './settingsmodal'
@@ -7,7 +7,7 @@ import SettingsModal from './settingsmodal'
 export default function MainHeader() {
     return (
         <Menu inverted stackable borderless fixed='top' size='huge' className='mainmenu' id='mainmenu'>
-            <Menu.Item header>Purple</Menu.Item>
+            <Menu.Item header as={Link} to='/'>Purple</Menu.Item>
             <Menu.Menu position='right'>
                 <Menu.Item>
                     <SettingsModal />
