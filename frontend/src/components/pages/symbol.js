78e623536e0e5160713fc5fec72248d826d924de
Thomas Gak Deluen <tgdn45@gmail.com>
2017-03-03 22:50:19 +0000

    Started symbol page

--- frontend/src/components/pages/symbol.js
+++ frontend/src/components/pages/symbol.js
@@ -6,21 +6,56 @@ import {
     Grid,
     Header,
     Menu,
+    Icon
 } from 'semantic-ui-react'
 
 import groupBy from 'lodash/groupBy'
 import map from 'lodash/map'
 
 class SymbolPage extends React.Component {
-    render() {
+    constructor(props) {
+        super(props)
+        const symbol = this.props.params.symbol
+        const symbols = this.props.symbols
+        for (let i = 0; i < symbols.length; i++) { // eslint-disable-line
+            console.log(symbols[i].name);
+            console.log(symbol);
+            if (symbols[i].name === symbol) {
+                return
+            }
+        }
+        this.doesNotExist = true
+    }
+
+    renderDoesNotExist() {
         return (
             <Grid padded stackable>
                 <Grid.Column width={16}>
-                    <Container>
-                        <Header as='h2'>{this.props.params.symbol}</Header>
+                    <Container fluid>
+                        <Header as='h2'>Symbol does not exist</Header>
                     </Container>
                 </Grid.Column>
             </Grid>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <Grid padded stackable>
+                    <Grid.Column width={16}>
+                        <Container fluid>
+                            <Menu size='huge'>
+                                <Menu.Item as={Link} to='/'>
+                                    <Icon name='arrow left' />
+                                    Return
+                                </Menu.Item>
+                            </Menu>
+                            <Header as='h2'>{this.props.params.symbol}</Header>
+                        </Container>
+                    </Grid.Column>
+                </Grid>
+            </div>
         )
     }
 }
