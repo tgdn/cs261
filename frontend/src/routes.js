d9d10c9bf51dfd38c3af12cc394cb09ba15edc59
Thomas Gak Deluen <tgdn45@gmail.com>
2017-03-06 18:53:47 +0000

    Added flagged page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,7 @@
 import App from './components/app'
 import HomePage from './components/pages/home'
 import SymbolPage from './components/pages/symbol'
+import FlaggedPage from './components/pages/flagged'
 import PageDoesNotExist from './components/pages/404'
 
 export default [
@@ -11,6 +12,16 @@ export default [
             component: HomePage,
         },
         childRoutes: [
+            {
+                path: 'flagged',
+                component: FlaggedPage,
+                childRoutes: [
+                    {
+                        path: ':alertid',
+                        component: null
+                    }
+                ]
+            },
             {
                 path: ':symbol',
                 component: SymbolPage,
