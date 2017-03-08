1e942d5624b049f7a2eb09ca7dfdde2d80794131
djtodd709 <djtodd709@gmail.com>
2017-03-08 15:43:50 +0000

    Overhauled - Errors likely but not certain

--- predictions.py
+++ predictions.py
@@ -19,54 +19,47 @@ def predict(symbol):
     timeAvg /= numTrades
     priceAvg /= numTrades
 
-    #calculate average time gap and standard deviation
+    #calculate average time gap
     timeGap = 0.0
-    priceGap = 0.0
     for i in range(1,numTrades):
         timeGap += (times[i]-times[i-1])
-        priceGap += (prices[i]-prices[i-1])**2
     timeGap /= numTrades
-    stDev = math.sqrt(priceGap/numTrades)
     
-    #gradient and intercept calculation
+    #gradient, intercept and standard deviation calculation
     numerator = 0.0
     denominator = 0.0
+    priceDev = 0.0
     for i in range(numTrades):
         numerator += (times[i]-timeAvg) * (prices[i]-priceAvg)
         denominator += (times[i]-timeAvg) ** 2
+        priceDev += (prices[i]-priceAvg)**2
     m = numerator / denominator
     c = priceAvg - (m*timeAvg)
-    
-##    #r squared calculation            UNCOMMENT FOR R2 REFINEMENT
-##    trendlineErr = 0.0
-##    priceErr = 0.0
-##    for i in range(numTrades):
-##        priceErr += (prices[i]-priceAvg) ** 2
-##        trendlineErr += ((m*times[i]+c)-priceAvg) ** 2
-##    r2 = trendlineErr/priceErr
+    stDev = math.sqrt(priceDev/numTrades)
 
     #Find co-ordinates for each prediction line
     DISTANCE = 10
-    xVal = times[numTrades-1] + timeGap
-    xValLast = xVal + timeGap*DISTANCE
-    yValLast = m*xValLast+c
+    xVal = times[numTrades-1]
     #datetime conversion
-    startTime = datetime.datetime.fromtimestamp(times[numTrades-1])
+    startTime = datetime.datetime.fromtimestamp(xVal)
     initialTime = datetime.datetime.fromtimestamp(times[0])
-    xValLast = datetime.datetime.fromtimestamp(xValLast)
     startPoint = (startTime,m*times[numTrades-1]+c)
-    ##DISTANCE *= (1-r2)         UNCOMMENT FOR R2 REFINEMENT
-    #compile together into list of points
+
     predictionLines = [[(initialTime,m*times[0]+c),startPoint],
-        [startPoint,(xValLast,yValLast + DISTANCE*(1*stDev))],
-        [startPoint,(xValLast,yValLast + DISTANCE*(2*stDev))],
-        [startPoint,(xValLast,yValLast + DISTANCE*(3*stDev))],
-        [startPoint,(xValLast,yValLast - DISTANCE*(1*stDev))],
-        [startPoint,(xValLast,yValLast - DISTANCE*(2*stDev))],
-        [startPoint,(xValLast,yValLast - DISTANCE*(3*stDev))]
-    ]
-    
+                       [startPoint], #min3
+                       [startPoint], #min2
+                       [startPoint], #min1
+                       [startPoint], #0
+                       [startPoint], #max1
+                       [startPoint], #max2
+                       [startPoint]] #max3
+    for p in range(-3,4):
+        for i in DISTANCE:
+            xVal += timeGap
+            xValTime = datetime.datetime.fromtimestamp(xVal)
+            predictionLines[p+4].append((xValTime,m*xVal+c+((math.sqrt(i)*p)*stDev)))
+        xVal = times[numTrades-1]
     return (predictionLines)
 
     #Prediction line consists of start and end co-ordinates of:
-    #[trendline, 1stDevUp, 2ndDevUp, 3rdDevUp, 1stDevDown, 2ndDevDown, 3rdDevDown]
+    #[trendline, 3rdDevDown, 2ndDevDown, 1stDevDown, trendlineCont, 1stDevUp, 2ndDevUp, 3rdDevUp]
