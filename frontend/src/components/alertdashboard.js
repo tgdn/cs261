972368d20bc408538b67f892fceca163cd37a219
Thomas Gak Deluen <tgdn45@gmail.com>
2017-03-09 11:28:31 +0000

    Started anomaly review system

--- frontend/src/components/alertdashboard.js
+++ frontend/src/components/alertdashboard.js
@@ -5,10 +5,22 @@ import {
     Icon,
     Menu,
     Segment,
-    Loader
+    Loader,
+    Button,
+    Label
 } from 'semantic-ui-react'
 
 import SymbolChart from './symbolchart'
+
+const severityToColor = (severity) => {
+    switch (severity) {
+    case 2: return 'yellow'
+    case 3: return 'blue'
+    case 1:
+    default:
+        return 'red'
+    }
+}
 
 const AlertDashboard = ({ alert, trades }) => (
     <div>
@@ -16,8 +28,15 @@ const AlertDashboard = ({ alert, trades }) => (
             <Menu.Item header>
                 <Icon name='warning sign' />
                 {alert.description}
+                <Label color={severityToColor(alert.severity)}>{alert.severity}</Label>
             </Menu.Item>
             <Menu.Menu position='right'>
+                <Menu.Item as={Button}>
+                    Review
+                </Menu.Item>
+                <Menu.Item as={Button}>
+                    Not an anomaly
+                </Menu.Item>
             </Menu.Menu>
         </Menu>
         <div>
